fe695a19d3264a8a758f139070b0be5d65ce4ee4
Quan Bao <quy123bao20@gmail.com>
2020-04-13 16:03:44 +0700

    Learning about method Post

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
 var express = require('express');
 var app = express();
 var port = 8080;
+var bodyParser = require('body-parser')
+var users = [
+    {id:1, name:'Lebron'},
+    {id:2, name:'Quan'}
+]
 
 app.set('view engine', 'pug')
 app.set('views', './views')
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 app.get('/',function(request,response){
-    response.render('index',{
+    response.render('index.pug',{
         name:'Quan'
     });
 })
 
 app.get('/users',function(request,response){
-    response.render('user',{
-        names:['Quan','Duy','Lebron']
+    response.render('user.pug',{
+        users:users
     });
 })
 
+app.get('/users/search', (request, response)=>{
+    var qu=request.query.q;
+    // console.log(request.query);
+    var matchedUsers = users.filter(data => data.name.toLocaleLowerCase().indexOf(qu.toLocaleLowerCase()) !== -1)
+    response.render('user.pug',{
+        users:matchedUsers,
+        nameInput:request.query.q
+    })
+})
+
+app.get('/users/create',function(request,response){
+    response.render('create.pug')
+})
+
+app.post('/users/create',(request,response)=>{
+    console.log(request.body);
+    var data= request.body['id']=users.length+1;
+    users.push(request.body);
+    response.redirect('/users');
+})
+
 app.listen(port,function(){
-    console.log('Server listening on port' + port);
+    console.log('Server listening on port ' + port);
 })
