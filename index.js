54833a40a351a483564291ef08fa40d95296e6c5
Quan Bao <quy123bao20@gmail.com>
2020-04-14 15:19:51 +0700

    set static file

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.set('view engine','pug');
 app.set('views', './views');
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('public'));
 
-var shortID = require('shortid')
 
 app.get('/',(request,response)=>{
     response.render('index.pug')
